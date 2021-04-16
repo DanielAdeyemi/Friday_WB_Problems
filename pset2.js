ca9205a47d33824dbb671f395cecb8f4b12777a5
DanielAdeyemi <adeyemidany@gmail.com>
2021-04-16 11:59:39 -0700

    solve problem with filter

--- pset2.js
+++ pset2.js
@@ -13,3 +13,10 @@ function duplicatesRemoval(arr) {
 }
 
 console.log(duplicatesRemoval([7, 9, "hi", 12, "hi", 7, 53]));
+
+arr = [7, 9, "hi", 12, "hi", 7, 53, 9, 8];
+const filteredArray = arr.filter(function(e, position) {
+  return arr.indexOf(e) === position;
+});
+// The logic for the callback function here is that if the indexOf value for current item is same as the index, it means the element has been encountered first time, so it can be considered unique. If not, it means the element has been encountered already, so should be discarded now.
+console.log(filteredArray);
