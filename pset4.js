6fc04eaa7e5d98572b21d815902365c8f88df05f
DanielAdeyemi <adeyemidany@gmail.com>
2021-04-16 14:03:48 -0700

    remove consolelogs

--- pset4.js
+++ pset4.js
@@ -1,8 +1,6 @@
 // Write an algorithm that determines whether all the elements in a string are unique. You may not convert the string into an string or use string methods to solve this problem. The algorithm should return a boolean.
 function uniqueStringChar(string) {
   let sSet = new Set(string);
-  console.log(sSet);
-  console.log(sSet.size);
   return (sSet.size == string.length);
 }
 
