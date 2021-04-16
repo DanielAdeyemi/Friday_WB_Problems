53af9dd9adabb5651d67dee9266565ff38445031
DanielAdeyemi <adeyemidany@gmail.com>
2021-04-16 14:23:27 -0700

    use selection sort to sort an array

--- pset5.js
+++ pset5.js
@@ -1,5 +1,5 @@
 // Write an algorithm that sorts an array without using the sort() method. 
-function sortArray(array) {
+function bubbleSortArray(array) {
   for (let i = 0; i < array.length; i++) {
     for (let j = i + 1; j < array.length; j++) {
       if (array[i] > array[j]) {
@@ -12,4 +12,20 @@ function sortArray(array) {
   return array;
 }
 
-console.log(sortArray([9, 2, 7, 12]));
+function selectionSortArray(array) {
+  let min = 0;
+  for (let i = 1; i < array.length; i++) {
+    if (array[min] > array[i]) {
+      temp = array[min];
+      array[min] = array[i];
+      array[i] = temp;
+    }
+    min++;
+  }
+  return array;
+}
+
+
+
+console.log(bubbleSortArray([9, 2, 7, 12]));
+console.log(selectionSortArray([9, 2, 7, 12]));
